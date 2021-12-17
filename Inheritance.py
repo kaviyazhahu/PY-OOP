6ee59f9241ab46b2e03e88ec50f6dd27f45de3f9
Ramakrishna <pammiramakrishna@gmail.com>
2021-12-17 17:05:18 +0530

    Polymorphism Concept Include

--- Inheritance.py
+++ Inheritance.py
@@ -13,15 +13,51 @@ class SoftwareDeveloper(Employee):
     def __init__(self, name, age, salary, level) -> None:
         super().__init__(name, age, salary)
         self.level = level    
+    def Coding(self):
+        print(f"{self.name} is coding ...")
+    
+    def work(self):
+        print(f"{self.name} is working ... from SD")
 
 class Designer(Employee):
-    pass
+
+    def drawing(self):
+        print(f"{self.name} is Drawing ...")
+    def work(self):
+        print(f"{self.name} is working... from DES")
+
+    
 
 se=SoftwareDeveloper("Kavi","29",29000,"J")
 print(se.name,se.age,se.salary)
 se.work()
-print(se.level)
+#print(se.level)
+se.Coding()
 
 d=Designer("Jhon","29",30000)
 print(d.name,d.age,d.salary)
 d.work()
+d.drawing()
+
+
+
+# Polymorphism
+
+print("\n +++ Polymorphism +++ \n")
+
+emp=[SoftwareDeveloper("Kavi","29",29000,"J"),
+     SoftwareDeveloper("Kavi","29",29000,"J"),
+     Designer("Jhon","29",30000)]    
+
+def motivate_emp(emp):
+    for list in emp:
+        list.work()
+
+motivate_emp(emp)
+
+
+# Recap
+# Inhertance : ChildClass(BaseClass)
+#Inherit,Extend,Override
+#Super().__init()__ 
+#Polymorphism
