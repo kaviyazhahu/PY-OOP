494f8918fd40725c9a8662ba941ddd88c4ca1082
Ramakrishna <pammiramakrishna@gmail.com>
2021-12-20 17:06:57 +0530

    Exception Handling

---
+++ ExfeptionHandling.py
@@ -0,0 +1,7 @@
+try:
+    num = int(input("Enter numerator: "))
+    den = int(input("Enter denominator: "))
+    result= num / den
+    print(result)
+except:
+    print("Denominatior cannot be a 0.")
